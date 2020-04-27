78d199c23b119a096b562b26288f224344c618cb
Augusto Telles <40339166+toguvr@users.noreply.github.com>
2020-04-27 19:34:06 -0300

    projeto pessoal

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,40 @@
-// const CACHE_NAME = 'raxa';
+const CACHE_NAME = 'static-v1';
 
-// const urlsToCache = ['/'];
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/app.js',
+        '/manifest.js',
+        '/vendor.js',
+      ]);
+    })
+  );
+});
 
-// self.addEventListener('install', function(event) {
-//   // Perform install steps
-//   event.waitUntil(
-//     caches.open(CACHE_NAME).then(function(cache) {
-//       return cache.addAll(urlsToCache);
-//     })
-//   );
-// });
+self.addEventListener('activate', function activator(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys
+          .filter(function(key) {
+            return key.indexOf(CACHE_NAME) !== 0;
+          })
+          .map(function(key) {
+            return caches.delete(key);
+          })
+      );
+    })
+  );
+});
 
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request).then(function(response) {
-//       // Cache hit - return response
-//       if (response) {
-//         return response;
-//       }
-//       return fetch(event.request);
-//     })
-//   );
-// });
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(cachedResponse) {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
