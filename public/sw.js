11e2d88ca19011865c775ae30db1523b962b9591
Augusto Telles <40339166+toguvr@users.noreply.github.com>
2020-04-27 00:53:49 -0300

    subindo projeto

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,24 @@
-const CACHE_NAME = 'raxa';
+// const CACHE_NAME = 'raxa';
 
-const urlsToCache = ['/'];
+// const urlsToCache = ['/'];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
+// self.addEventListener('install', function(event) {
+//   // Perform install steps
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then(function(cache) {
+//       return cache.addAll(urlsToCache);
+//     })
+//   );
+// });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    })
-  );
-});
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request).then(function(response) {
+//       // Cache hit - return response
+//       if (response) {
+//         return response;
+//       }
+//       return fetch(event.request);
+//     })
+//   );
+// });
